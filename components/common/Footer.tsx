5371df4882bd697e71caf0aa658e99ca4c440331
Stefano Satta <Pippo@mbpss.homenet.telecomitalia.it>
2021-10-30 12:18:54 +0200

    added contact linkedin and mail to footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,12 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { FC } from "react";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
 
 const Footer: FC = () => {
 
     return (<>
         <footer className="footer text-center bg-primary text-white py-4">
             <h3>Cryptoboard</h3>
+            
+            <div className="mb-3">
+                <a href="mailto:stefanosatta@outlook.com" rel="noreferrer">
+                    <FontAwesomeIcon icon={'envelope'} className="me-3" size="lg"/>
+                </a>
+                <a href="https://linkedin.com/in/stefano-satta" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={['fab', 'linkedin-in']} className="me-3" size="lg"/>
+                </a>
+            </div>
             <p className="mb-0">&copy; Stefano Satta 2021</p>
-            <p className="mb-0"><small>Made it with &hearts;</small></p>
+            <p className="mb-0"><small>Made it with NextJS and RapidAPI</small></p>
         </footer>
     </>)
 }
